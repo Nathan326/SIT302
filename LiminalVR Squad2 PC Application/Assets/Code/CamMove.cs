a428d900bd831fb3d9d187e7a1150eb75c275500
Nathan326 <50762422+Nathan326@users.noreply.github.com>
2019-09-24 12:52:46 +1000

    Working demo
    The working demo

---
+++ LiminalVR Squad2 PC Application/Assets/Code/CamMove.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CamMove : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void ResetCamera()
+    {
+        transform.eulerAngles = new Vector3(0, 0, 0);
+    }
+    float speed;
+
+    void Start()
+    {
+        speed = 10f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.eulerAngles = new Vector3(transform.eulerAngles.x - 2, transform.eulerAngles.y, transform.eulerAngles.z);
+        }
+
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.eulerAngles = new Vector3(transform.eulerAngles.x + 2, transform.eulerAngles.y, transform.eulerAngles.z);
+        }
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.eulerAngles = new Vector3(transform.eulerAngles.x, transform.eulerAngles.y - 2, transform.eulerAngles.z);
+        }
+
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.eulerAngles = new Vector3(transform.eulerAngles.x, transform.eulerAngles.y + 2, transform.eulerAngles.z);
+        }
+    }
+}
---
+++ LiminalVR Squad2 PC Application/Assets/Code/Crystal Spins/CrystalSpin7.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CrystalSpin7 : MonoBehaviour
+{
+    public bool rotateObject = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<Behaviour>().enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit))
+        {
+            if (hit.collider.gameObject.name == "Crystal7")
+            {
+                if (Input.GetMouseButtonDown(0))
+                {
+                    if (rotateObject == true)
+                    {
+                        rotateObject = false;
+                    }
+                    else
+                    {
+                        rotateObject = true;
+                    }
+                    if (GetComponent<Behaviour>().enabled == false)
+                    {
+                        GetComponent<Behaviour>().enabled = true;
+                    }
+                    else
+                    {
+                        GetComponent<Behaviour>().enabled = false;
+                    }
+                }
+
+            }
+        }
+        else
+        {
+            Debug.Log("This isn't a Player");
+        }
+        if (rotateObject == true)
+        {
+            transform.eulerAngles = new Vector3(transform.eulerAngles.x, transform.eulerAngles.y + 0.5f, transform.eulerAngles.z);
+        }
+    }
+
+}
+
+
+
+
